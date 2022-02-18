54d3cb121c93961bbbded7e3f9283d46f120ba4d
Dom <domant3lis@protonmail.com>
2022-02-18 11:52:44 +0200

    Final changes

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -2,62 +2,99 @@ import java.io.File; // Must use
 import java.util.ArrayList;
 import java.util.List;
 
-class MyFileUtil
-{
-	// static public List<MyFile> getFilesWithExt(MyFile dir, String ext)
-	// {
-		
+// Show all files recursively with a certain file extension
+public class Lab1 {
+	static final private String OS = System.getProperty("os.name");
+	static final private char SEPARATOR = OS.equals("win") ? '\\' : '/';
 
-	// 	// return ret;
-	// }
-}
+	public static void main(String[] args) {
 
-public class Lab1
-{
-	public static void main(String[] args)
-	{
-
-		MyFile dir;
 		String ext;
 
-		// Via command line 
-		if (args.length == 2)
-		{
-			dir = new MyFile(args[0]);
+		if (args.length == 2) {
 			ext = args[1];
-			MyFile.getFileTree(dir, 0).forEach(System.out::println );
-		}
-		else
-		{
+
+			// Gets all files
+			List<File> fileList = Lab1.getFilesRec(args[0], 0);
+
+			// Prints all files2
+			System.out.println("Showing files ending in \"." + ext + "\"");
+			for (File file : fileList) {
+				if (file.isDirectory())
+					System.out.println("--" + file);
+
+				if (Lab1.fileExtension(file.getName(), SEPARATOR).equals(ext))
+					System.out.println("  " + file);
+			}
+
+		} else {
 			System.out.println("Not enough arguments");
 		}
-
-
-		// Via GUI
-		// else
-		// {
-		// 	dir = new MyFile("test0");
-		// 	ext = "txt";
-		// }
-
-		// List<MyFile> ret = ;
-		
-		// for (String fileName : dir.list())
-		// {
-		// 	MyFile file = new MyFile(); 
-		// 	if () 
-		// 	try {
-		// 		if (MyFile.fileExtension(file).equals(ext))
-		// 		{
-		// 			ret.add(new MyFile(file));
-		// 		}
-		// 	} catch (FileIsDirectoryException fileIsDir) {
-		// 		System.out.println("TO HANDLE");;
-		// 	}
-		// }
-
-		// List<File> files = MyFileUtil.getFilesWithExt(dir, ext);
-
-		// files.forEach(System.out::println);
 	}
+
+	// Gets a list of all files within a `root` directory
+	static public List<File> getFilesRec(String rootPath, int level) {
+		File root = new File(rootPath);
+
+		List<File> ret = new ArrayList<File>();
+
+		for (String fileName : root.list()) {
+			File file = new File(fileName);
+
+			ret.add(file);
+
+			if (file.isDirectory()) {
+				Lab1.getFilesRec(file.getAbsolutePath(), level + 1).forEach(el -> ret.add(el));
+			}
+		}
+
+		return ret;
+	}
+
+	// IMPORTANT: Does not support multi-part extensions, like `tar.xz`
+	static private String fileExtension(String fileName, char separator) {
+		// Gets file extension in reverse order
+		List<Character> extReversed = new ArrayList<Character>();
+		int fIter;
+		for (fIter = fileName.length() - 1; fIter > 0; --fIter) {
+			char ch = fileName.charAt(fIter);
+
+			if (ch == '.') {
+				// Detects hidden UNIX files
+				// by seeing if '.' is the first symbol
+				if (fIter == 0)
+					return new String("");
+
+				break;
+			}
+			// then no '.' symbol is found
+			// it is assumed that the file has no extension
+			else if (ch == separator) /// asd/asd
+				return new String("");
+
+			extReversed.add(ch);
+		}
+
+		if (fIter == 0)
+			return new String("");
+
+		// Reverses into correct form
+		// and constructs a String from List of Characters
+		char[] ret = new char[extReversed.size()];
+		for (int iter = extReversed.size() - 1, at = 0; iter >= 0; --iter, ++at)
+			ret[at] = extReversed.get(iter);
+
+		return new String(ret);
+	}
+
+	// static public String stringMul(String str, int times) {
+	// if (times == 0)
+	// return new String("");
+
+	// for (int iter = 0; iter < times; ++iter) {
+	// str += str;
+	// }
+	// return str;
+	// }
+
 }
